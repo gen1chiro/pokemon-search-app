f099062828444944635017dd2f414900fae29192
gen1chiro <juljavellana1218@gmail.com>
2024-11-20 15:37:16 +0800

    Add updateColorBasedOnType & clearUi functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 const spriteContainer = document.getElementById("sprite-container");
+const upperContainer = document.querySelector(".upper");
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
 const weight = document.getElementById("weight");
@@ -21,12 +22,14 @@ const fetchData = async (input) => {
         updateStats(data);
     } catch (err) {
         alert("Pokémon not found");
+        updateColorBasedOnType(upperContainer,"base");
+        clearUi();
     }
 }
 
 const updateStats = (data) => {
-    pokemonName.textContent = data.name;
-    pokemonId.textContent = data.id;
+    pokemonName.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+    pokemonId.textContent = `NO. ${data.id}`;
     weight.textContent = data.weight;
     height.textContent = data.height;
     updateSprite(data.sprites.front_default);
@@ -38,12 +41,33 @@ const updateStats = (data) => {
     speed.textContent = data.stats[5].base_stat;
 
     data.types.forEach(type => {
-        types.innerHTML += `<span>${type.type.name}</span>`;
-    })
+        types.innerHTML += `<p class="type ${type.type.name}">${type.type.name}</p>`;
+    });
+
+    updateColorBasedOnType(upperContainer, data.types[0].type.name);
 }
 
 const updateSprite = (src) => {
     spriteContainer.innerHTML = `<img id="sprite" src="${src}" alt="sprite" class="pokemon-sprite">`
+}
+
+const updateColorBasedOnType = (element, type) => {
+    element.className = "";
+    element.className = `upper ${type}`;
+}
+
+const clearUi = () => {
+    pokemonName.textContent = "";
+    pokemonId.textContent = "";
+    weight.textContent = "";
+    height.textContent = "";
+    hp.textContent = "";
+    attack.textContent = "";
+    defense.textContent = "";
+    specialAttack.textContent = "";
+    specialDefense.textContent = "";
+    speed.textContent = "";
+    spriteContainer.innerHTML = "";
 }
 
 const clearTypes = () => {
@@ -53,6 +77,14 @@ const clearTypes = () => {
 searchBtn.addEventListener("click", () => {
     clearTypes();
     fetchData(searchInput.value.toLowerCase());
+});
+
+searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        clearTypes();
+        fetchData(searchInput.value.toLowerCase());
+    }
 });
 
 
