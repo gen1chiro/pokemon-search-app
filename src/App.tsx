0ffde93fd74a8f34fcdf3cec3181dea5c6e16f54
gen1chiro <juljavellana1218@gmail.com>
2025-04-25 16:15:12 +0800

    initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react'
-import { RawApiResponse, FilteredApiResponse, Stats, Types} from "../types/types.ts";
+import { RawApiResponse, FilteredApiResponse, Stats, Types} from './types/types.ts'
+import Default from './components/Default.tsx'
 
 function App() {
     const [pokemonData, setPokemonData] = useState<FilteredApiResponse | null>(null)
@@ -45,12 +46,22 @@ function App() {
     }
 
     return (
-        <main>
-            <input ref={inputRef} type="text" onKeyDown={handleKeyDown}/>
-            <button onClick={handleClick}>Search</button>
-            <div>
-                {pokemonData ? JSON.stringify(pokemonData) : "none"}
-            </div>
+        <main className="w-5/6 mx-auto mt-8 max-w-xl pt-8 pb-2 font-sans bg-gray-100 rounded-3xl shadow-lg">
+            <section className="max-w-sm mx-auto flex items-center gap-2 border-1 p-1 bg-white shadow-lg border-white rounded-full">
+                <input ref={inputRef} type="text" onKeyDown={handleKeyDown}
+                    className="flex-grow focus:outline-none px-3"
+                />
+                <button onClick={handleClick}
+                    className="bg-gradient-to-br from-blue-400 to-blue-600 shadow-lg rounded-full aspect-square p-2 hover:scale-105 transition-all ease-in"
+                >
+                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+                            stroke="#FFFFFF" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </button>
+            </section>
+            {pokemonData ? JSON.stringify(pokemonData) : <Default/>}
         </main>
     )
 }
---
+++ src/components/Default.tsx
@@ -0,0 +1,10 @@
+const Default = () => {
+    return (
+        <div className="bg-white rounded-xl m-4 flex flex-col justify-center items-center p-8 gap-2 font-inter">
+            <h1 className="text-xl font-bold text-center">Use the search bar above to search for pokemon using their name or ID!</h1>
+            <p>Results will appear here</p>
+        </div>
+    )
+}
+
+export default Default
